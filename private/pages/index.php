0929e5af081dc9422458a8a4ce30a4d94ac972aa
Bluffingo <chazpelo2@gmail.com>
2024-03-05 15:28:48 -0500

    fix

--- private/pages/index.php
+++ private/pages/index.php
@@ -5,34 +5,17 @@ global $database;
 
 use Site\Templating;
 
-//array(5) {
-//  ["softwareInput"]=>
-//  string(7) "firefox"
-//  ["versionInput"]=>
-//  string(7) "5345345"
-//  ["downloadInput"]=>
-//  string(6) "453543"
-//  ["releasedInput"]=>
-//  string(10) "0555-05-05"
-//  ["addButton"]=>
-//  string(14) "AddNewDownload"
-//}
-
-//INSERT INTO versions (int_id, app_id, version, released, url) VALUES ('2', '3', '534534', '534543', '543345435');
-
 if (isset($_POST["addButton"])) {
     if ($_POST["addButton"] == "AddNewDownload") {
         $database->query("INSERT INTO versions (app_id, version, released, url) VALUES (?,?,?,?)",
             [$_POST["softwareInput"], $_POST["versionInput"], $_POST["releasedInput"], $_POST["downloadInput"]]);
-    }
-
-    if ($_POST["addButton"] == "AddNewApplication") {
-        if ($database->query("SELECT * FROM software WHERE app_id = ?", $_POST["internalIDInput"]))
+    } elseif ($_POST["addButton"] == "AddNewApplication") {
+        if ($database->fetch("SELECT * FROM software WHERE app_id = ?", [$_POST["internalIDInput"]]))
         {
             die("This ID has already been used.");
         }
 
-        if ($database->query("SELECT * FROM software WHERE name = ?", $_POST["appNameInput"]))
+        if ($database->fetch("SELECT * FROM software WHERE name = ?", [$_POST["appNameInput"]]))
         {
             die("This application has already been added.");
         }
